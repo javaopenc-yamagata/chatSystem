ae1f5a964c679d28e9a1fd5d2168755b1b87258e
yamagata <you@example.com>
2025-05-23 10:52:22 +0900

    名前、コメントの打ち込み画面の表示（画面上のみ）

--- src/main/java/com/example/demo/form/ChatController.java
+++ src/main/java/com/example/demo/form/ChatController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ChatController {
 	
-	@RequestMapping("/Hello")
+	@RequestMapping("/hello")
 		public String Hello(Model model) {
 		model.addAttribute("message", "Hello!");
 		return "sample";
 	}
 	
+	@RequestMapping("/form")
+	public String Form(Model model) {
+		return "form/index";
+		
+	}
 	
 	
 }
