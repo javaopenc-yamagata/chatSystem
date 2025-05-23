0771352a2e526a5de35f118f46a039ac969e72ba
yamagata <you@example.com>
2025-05-23 11:56:02 +0900

    トップページ、一覧ページと投稿画面への移動（一覧表示まだ）

--- src/main/java/com/example/demo/form/Chat.java
+++ src/main/java/com/example/demo/form/Chat.java
@@ -2,4 +2,23 @@ package com.example.demo.form;
 
 public class Chat {
 
+	private String name;
+	private String comment;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
 }
--- src/main/java/com/example/demo/form/ChatController.java
+++ src/main/java/com/example/demo/form/ChatController.java
@@ -7,16 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ChatController {
 	
-	@RequestMapping("/hello")
+	/*@RequestMapping("/hello")
 		public String Hello(Model model) {
-		model.addAttribute("message", "Hello!");
+		model.addAttribute("message", "【Chat】へようこそ！");
 		return "sample";
+	}*/
+	
+	@RequestMapping("/hello")
+	public String Form(Model model) {
+		model.addAttribute("message", "【Chat】へようこそ！");
+		return "form/index";
+		
 	}
 	
 	@RequestMapping("/form")
-	public String Form(Model model) {
-		return "form/index";
-		
+	public String Confirm(Model model, Chat chat) {
+		model.addAttribute("message","このように投稿しました！");
+		return "form/view";
 	}
 	
 	
